4d1094c96b173448432d6ffe71b70671a2a6e020
Syméon RB <dev.chresyr@gmail.com>
2023-11-08 12:16:03 +0100

    Improved signin ui

--- lib/route/home/screen/profile_screen.dart
+++ lib/route/home/screen/profile_screen.dart
@@ -30,34 +30,244 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<AuthenticationCubit, AuthenticationModel>(
-      builder: (context, state) {
+      builder: (context, auth) {
         return Center(
-          child: state.isAbsent
-              ? ElevatedButton(
-                  onPressed: AuthService.signInWithGoogle,
-                  child: Image.asset(
-                    'assets/icon/google_icon.png',
-                    height: 100,
-                  ),
-                )
-              : Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    CircleAvatar(
-                      radius: 50,
-                      backgroundImage: NetworkImage(state.photo ?? ''),
-                    ),
-                    Text(state.email ?? ''),
-                    ElevatedButton.icon(
-                      onPressed:
-                          context.read<AuthenticationCubit>().signoutRequested,
-                      icon: const Icon(Icons.logout),
-                      label: const Text('Se déconnecter'),
-                    ),
-                  ],
-                ),
+          child: auth.isAbsent ? _SigninScreen() : const _ProfileScreen(),
         );
       },
+    );
+  }
+}
+
+class _SigninScreen extends StatelessWidget {
+  _SigninScreen();
+
+  // text editing controllers
+  final usernameController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        const SizedBox(height: 50),
+
+        // logo
+        const Icon(
+          Icons.lock,
+          size: 100,
+        ),
+
+        const SizedBox(height: 50),
+
+        // welcome back, you've been missed!
+        Text(
+          'Welcome back you\'ve been missed!',
+          style: TextStyle(
+            color: Colors.grey[700],
+            fontSize: 16,
+          ),
+        ),
+
+        const SizedBox(height: 25),
+
+        // username textfield
+        MyTextField(
+          controller: usernameController,
+          hintText: 'Username',
+          obscureText: false,
+        ),
+
+        const SizedBox(height: 10),
+
+        // password textfield
+        MyTextField(
+          controller: passwordController,
+          hintText: 'Password',
+          obscureText: true,
+        ),
+
+        const SizedBox(height: 10),
+
+        // forgot password?
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Text(
+                'Forgot Password?',
+                style: TextStyle(color: Colors.grey[600]),
+              ),
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 25),
+
+        // sign in button
+        FilledButton(
+          onPressed: signUserIn,
+          child: const Text('Sign in'),
+        ),
+
+        const SizedBox(height: 50),
+
+        // or continue with
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: Divider(
+                  thickness: 0.5,
+                  color: Colors.grey[400],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                child: Text(
+                  'Or continue with',
+                  style: TextStyle(color: Colors.grey[700]),
+                ),
+              ),
+              Expanded(
+                child: Divider(
+                  thickness: 0.5,
+                  color: Colors.grey[400],
+                ),
+              ),
+            ],
+          ),
+        ),
+
+        const SizedBox(height: 50),
+
+        // google + apple sign in buttons
+        const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // google button
+            SquareTile(imagePath: 'assets/icon/google_icon.png'),
+
+            SizedBox(width: 25),
+
+            // apple button
+            SquareTile(imagePath: 'assets/icon/apple_icon.png')
+          ],
+        ),
+
+        const SizedBox(height: 50),
+
+        // not a member? register now
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Not a member?',
+              style: TextStyle(color: Colors.grey[700]),
+            ),
+            const SizedBox(width: 4),
+            const Text(
+              'Register now',
+              style: TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class _ProfileScreen extends StatelessWidget {
+  const _ProfileScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AuthenticationCubit, AuthenticationModel>(
+      builder: (context, auth) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(auth.photo ?? ''),
+            ),
+            Text(auth.email ?? ''),
+            ElevatedButton.icon(
+              onPressed: context.read<AuthenticationCubit>().signoutRequested,
+              icon: const Icon(Icons.logout),
+              label: const Text('Se déconnecter'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class SquareTile extends StatelessWidget {
+  final String imagePath;
+  const SquareTile({
+    super.key,
+    required this.imagePath,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.white),
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.grey[200],
+      ),
+      child: Image.asset(
+        imagePath,
+        height: 40,
+      ),
+    );
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  final TextEditingController? controller;
+  final String hintText;
+  final bool obscureText;
+
+  const MyTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            enabledBorder: const OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.white),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+            ),
+            fillColor: Colors.grey.shade200,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.grey[500])),
+      ),
     );
   }
 }
